8d89cdd71efc6de731c261ff6978d640c4eba2f5
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-04-30 17:57:40 +0000

    adding a dummy TextOverlayFilter for experimenting
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/community/modules/magnolia-module-imagefiltering/trunk@24651 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-imaging/src/main/java/info/magnolia/imaging/filters/text/TextOverlayImageFilter.java
@@ -0,0 +1,103 @@
+/**
+ * This file Copyright (c) 2003-2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.imaging.filters.text;
+
+import info.magnolia.cms.core.Content;
+import info.magnolia.imaging.filters.ImageFilter;
+
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.font.GlyphVector;
+import java.awt.geom.Point2D;
+import java.awt.image.BufferedImage;
+
+/**
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class TextOverlayImageFilter implements ImageFilter<TextStyle> {
+
+    public BufferedImage apply(BufferedImage src, TextStyle filterParams, Content dialogControlConfigNode) {
+//        final ColorModel cm = src.getColorModel();
+//        final WritableRaster raster = cm.createCompatibleWritableRaster(src.getWidth(), src.getHeight());
+//        final BufferedImage dest = new BufferedImage(cm, raster, cm.isAlphaPremultiplied(), null);
+
+        renderText(src.createGraphics(), "Hello world", 10, src.getHeight() - 10);
+
+        return src;
+    }
+
+    // roughly copied from texticons-BackgroundImageRenderer
+    protected void renderText(Graphics2D g, final String txt, final int txtPositionX, final int txtPositionY) {
+
+        final TextStyle style = new TextStyle();
+        style.setFont(new Font("Arial", 1, 20));
+        style.setColor(Color.RED);
+        // shear(g, style.getShearingValue());
+
+        g.setFont(style.getFont());
+        g.setColor(style.getColor());
+//        FontMetrics fontmetrics = g.getFontMetrics();
+//        int lineHeight = fontmetrics.getHeight();
+        // int shearingDelta = new Double(lineHeight * style.getShearingValue()).intValue();
+        // int newX = new Double(txtPositionX - shearingDelta - style.getShearingValue() * txtPositionY).intValue();
+        drawString(g, style, txt, txtPositionX, txtPositionY);
+
+        // undo transformation (pseudo-italic)
+        // shear(g, -1 * style.getShearingValue());
+
+    }
+
+    protected synchronized void drawString(Graphics2D graphics2d,
+                                           TextStyle style, String text, int txtPositionX, int txtPositionY) {
+        float f = 0.0F;
+        Font font = style.getFont();
+        graphics2d.setFont(font);
+        graphics2d.setColor(style.getColor());
+        FontMetrics fontmetrics = graphics2d.getFontMetrics();
+        float f1 = (float) ((double) style.getCharSpacing() / TextStyle.CHARSPACING_DIV_FACTOR);
+        FontRenderContext fontrendercontext = new FontRenderContext(null, false, false);
+        GlyphVector glyphvector = font.createGlyphVector(fontrendercontext, text);
+        for (int k = 0; k < text.length(); k++) {
+            char c = text.charAt(k);
+            int l = fontmetrics.charWidth(c);
+            Point2D point2d = glyphvector.getGlyphPosition(k);
+            point2d.setLocation(f, point2d.getY());
+            glyphvector.setGlyphPosition(k, point2d);
+            f += (new Float((float) l + f1)).floatValue();
+        }
+        graphics2d.drawGlyphVector(glyphvector, (float) txtPositionX, (float)txtPositionY + style.getFontSize());
+    }
+
+}
---
+++ magnolia-module-imaging/src/main/java/info/magnolia/imaging/filters/text/TextStyle.java
@@ -0,0 +1,120 @@
+/**
+ * This file Copyright (c) 2003-2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This file is dual-licensed under both the Magnolia
+ * Network Agreement and the GNU General Public License.
+ * You may elect to use one or the other of these licenses.
+ *
+ * This file is distributed in the hope that it will be
+ * useful, but AS-IS and WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE, TITLE, or NONINFRINGEMENT.
+ * Redistribution, except as permitted by whichever of the GPL
+ * or MNA you select, is prohibited.
+ *
+ * 1. For the GPL license (GPL), you can redistribute and/or
+ * modify this file under the terms of the GNU General
+ * Public License, Version 3, as published by the Free Software
+ * Foundation.  You should have received a copy of the GNU
+ * General Public License, Version 3 along with this program;
+ * if not, write to the Free Software Foundation, Inc., 51
+ * Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * 2. For the Magnolia Network Agreement (MNA), this file
+ * and the accompanying materials are made available under the
+ * terms of the MNA which accompanies this distribution, and
+ * is available at http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.imaging.filters.text;
+
+import java.awt.*;
+
+
+/**
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ *          TODO : copied from org.mgnl.magnolia.texticons.utils
+ */
+public class TextStyle {
+    public static final double SHEARING_DIV_FACTOR = 20D;
+    public static final double CHARSPACING_DIV_FACTOR = 10D;
+
+    private Font font;
+    private int fontStyle = Font.PLAIN;
+    private Color color;
+    private Color bgColor;
+    private double charSpacing;
+    private int fontSize;
+    private int shearingValue;
+
+    public double calculateShearingValue() {
+        return (-1.0D * (new Double(shearingValue)).doubleValue()) / SHEARING_DIV_FACTOR;
+    }
+
+    public Font deriveFont() {
+        return this.font.deriveFont(this.fontStyle, (float) this.fontSize);
+    }
+
+    // ---generated getters and setters
+    public double getCharSpacing() {
+        return charSpacing;
+    }
+
+    public void setCharSpacing(double charSpacing) {
+        this.charSpacing = charSpacing;
+    }
+
+    public Font getFont() {
+        return font;
+    }
+
+    public void setFont(Font font) {
+        this.font = font;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public int getFontSize() {
+        return fontSize;
+    }
+
+    public void setFontSize(int fontSize) {
+        this.fontSize = fontSize;
+    }
+
+    public int getShearingValue() {
+        return shearingValue;
+    }
+
+    public void setShearingValue(int shearingValue) {
+        this.shearingValue = shearingValue;
+    }
+
+    public int getFontStyle() {
+        return fontStyle;
+    }
+
+    public void setFontStyle(int fontStyle) {
+        this.fontStyle = fontStyle;
+    }
+
+    public Color getBgColor() {
+        return bgColor;
+    }
+
+    public void setBgColor(Color bgColor) {
+        this.bgColor = bgColor;
+    }
+}
