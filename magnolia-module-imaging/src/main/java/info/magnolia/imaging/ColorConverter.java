0de75a20eca1ad32118f47eddb3c04d5efb86d1a
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-05-04 20:44:19 +0000

    converting colors...
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/enterprise/imaging/trunk@24710 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-imaging/src/main/java/info/magnolia/imaging/ColorConverter.java
@@ -0,0 +1,76 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.imaging;
+
+import java.awt.Color;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * A class that is able to convert java.lang.String in various formats
+ * to java.awt.Color instances.
+ * TODO - and vice-versa.
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class ColorConverter {
+    private static final Map<String, Color> namedColors = reflectivelyGetNamedColors();
+
+    public static Color toColor(String s) {
+        if (namedColors.containsKey(s)) {
+            return namedColors.get(s);
+        } else if (s.startsWith("#")) {
+            return Color.decode(s);
+        } else {
+            throw new IllegalArgumentException("Can't decode color: " + s + ". Please provide either an #ffffff hexadecimal value or a know named color.");
+        }
+    }
+
+//    public static String toString(Color c) {
+//        System.out.println("c.getRGB() = " + c.getRGB());
+//        return null;
+//    }
+
+    /**
+     * Using reflection, gather all fields of java.awt.Color that are
+     * static, public, final and of type java.awt.Color, and put them
+     * in them in a case-insensitive Map.
+     * Since we've already loaded the java.awt.Color, there is virtually
+     * no hit on performance or memory usage, except for this very small
+     * map.
+     * Yes, this is a bit ugly, since we're relying on a convention..
+     * .. but it is not very likely to change.
+     */
+    private static Map<String, Color> reflectivelyGetNamedColors() {
+        try {
+            final Map<String, Color> namedColors = new TreeMap<String, Color>(String.CASE_INSENSITIVE_ORDER);
+            final Field[] fields = Color.class.getFields();
+            for (final Field field : fields) {
+                int mod = field.getModifiers();
+                if (Modifier.isStatic(mod) && Modifier.isPublic(mod) && Modifier.isFinal(mod)) {
+                    if (Color.class.equals(field.getType())) {
+                        namedColors.put(field.getName(), (Color) field.get(null));
+                    }
+                }
+            }
+            return namedColors;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Can't access field values of java.awt.Color, is this system too secure?", e);
+        }
+    }
+}
---
+++ magnolia-module-imaging/src/test/java/info/magnolia/imaging/ColorConverterTest.java
@@ -0,0 +1,58 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.imaging;
+
+import junit.framework.TestCase;
+
+import java.awt.Color;
+
+/**
+ * TODO : accept formats such as "255, 0, 0" ?
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class ColorConverterTest extends TestCase {
+    public void testCanDecodeHexa() {
+        assertEquals(Color.white, ColorConverter.toColor("#ffffff"));
+        assertEquals(Color.black, ColorConverter.toColor("#000000"));
+        assertEquals(Color.red, ColorConverter.toColor("#ff0000"));
+        assertEquals(new Color(128, 64, 23), ColorConverter.toColor("#804017"));
+    }
+
+//    public void testCanDecodeHexaWithAlphaChannel() {
+//        assertEquals(new Color(128, 64, 23, 35), ColorConverter.toColor("#80401723"));
+//    }
+
+    public void testCanDecodeNames() {
+        assertEquals(Color.white, ColorConverter.toColor("white"));
+        assertEquals(Color.black, ColorConverter.toColor("Black"));
+        assertEquals(Color.red, ColorConverter.toColor("RED"));
+        assertEquals(Color.yellow, ColorConverter.toColor("yelLoW"));
+    }
+
+    public void testFailsOnUnknownName() {
+        try {
+            ColorConverter.toColor("foo");
+            fail("should have failed");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Can't decode color: foo. Please provide either an #ffffff hexadecimal value or a know named color.", e.getMessage());
+        }
+    }
+
+//    public void testCanEncodeToHexa() {
+//        assertEquals("#0000ff", ColorConverter.toString(Color.blue));
+//    }
+}
