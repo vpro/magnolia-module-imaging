755e59e5eb21c2caf540aed1265d0fd5135e099b
Philipp Bärfuss <philipp.baerfuss@magnolia-cms.com>
2009-06-08 12:57:31 +0000

    MGNLIMG-54: give anonymous role write permission on public instances
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/enterprise/imaging/trunk@25961 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

--- magnolia-module-imaging/src/main/java/info/magnolia/imaging/setup/ImagingModuleVersionHandler.java
+++ magnolia-module-imaging/src/main/java/info/magnolia/imaging/setup/ImagingModuleVersionHandler.java
@@ -14,12 +14,37 @@
  */
 package info.magnolia.imaging.setup;
 
+import java.util.Collections;
+import java.util.List;
+
+import info.magnolia.cms.security.Permission;
+import info.magnolia.cms.security.Role;
+import info.magnolia.cms.security.Security;
 import info.magnolia.module.DefaultModuleVersionHandler;
+import info.magnolia.module.InstallContext;
+import info.magnolia.module.delta.AbstractTask;
+import info.magnolia.module.delta.IsAuthorInstanceDelegateTask;
+import info.magnolia.module.delta.TaskExecutionException;
+
 
 /**
- *
  * @author gjoseph
  * @version $Revision: $ ($Author: $)
  */
 public class ImagingModuleVersionHandler extends DefaultModuleVersionHandler {
+
+    @Override
+    protected List getExtraInstallTasks(InstallContext installContext) {
+        return Collections.singletonList(new IsAuthorInstanceDelegateTask(
+            "Give anynimous role permission to write/cache images.",
+            "The anonymous user needs write permission so that images can be cached on public instances",
+            null,
+            new AbstractTask("", "") {
+
+                public void execute(InstallContext installContext) throws TaskExecutionException {
+                    Role anonymous = Security.getRoleManager().getRole("anonymous");
+                    anonymous.addPermission("imaging", "/*", Permission.ALL);
+                }
+            }));
+    }
 }
