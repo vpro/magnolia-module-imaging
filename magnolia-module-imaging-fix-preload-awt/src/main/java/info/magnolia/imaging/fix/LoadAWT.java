8b1bd05cb6440bead6cae7b6053660c9cedc8ad1
Grégory Joseph <gregory.joseph@magnolia-cms.com>
2009-06-25 17:21:26 +0000

    MGNLIMG-55 workaround, simple module that ensures the AWT library is preloaded
    git-svn-id: file:///home/dlipp/subversion-migration/svn-repo-copy/enterprise/imaging/trunk@26446 a15fbcf9-bee4-0310-bafe-d02d26ad3b5f

---
+++ magnolia-module-imaging-fix-preload-awt/src/main/java/info/magnolia/imaging/fix/LoadAWT.java
@@ -0,0 +1,38 @@
+/**
+ * This file Copyright (c) 2009 Magnolia International
+ * Ltd.  (http://www.magnolia-cms.com). All rights reserved.
+ *
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Magnolia Network Agreement
+ * which accompanies this distribution, and is available at
+ * http://www.magnolia-cms.com/mna.html
+ *
+ * Any modifications to this file must keep this entire header
+ * intact.
+ *
+ */
+package info.magnolia.imaging.fix;
+
+import info.magnolia.module.ModuleLifecycle;
+import info.magnolia.module.ModuleLifecycleContext;
+
+import java.awt.Toolkit;
+
+/**
+ *
+ * @author gjoseph
+ * @version $Revision: $ ($Author: $)
+ */
+public class LoadAWT implements ModuleLifecycle {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(LoadAWT.class);
+
+    public void start(ModuleLifecycleContext moduleLifecycleContext) {
+        log.info("Starting up" + this);
+        final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+        log.info("Lib loaded, defaultToolkit is " + defaultToolkit);
+    }
+
+    public void stop(ModuleLifecycleContext moduleLifecycleContext) {
+    }
+}
